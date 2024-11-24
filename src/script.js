c7cd7c359e81eddc30e2ec6381370e3acef12398
Nasir6276 <92574698+Nasir6276@users.noreply.github.com>
2024-11-24 23:04:43 +0100

    funtionalities

--- src/script.js
+++ src/script.js
@@ -1,24 +1,39 @@
+const state = {
+    openDrawer: null,
+    currencies: []
+}
+
 // selectors
 const ui = {
     controls: document.getElementById('controls'),
     drawer: document.getElementById('drawer'),
     dismissBtn: document.getElementById('dismiss-btn'),
+    currencyList: document.getElementById('currency-list')
 }
 
 // event listeners
 
 const setUpEventListeners = () => {
+    document.addEventListener('DOMContentLoaded', initApp)
     ui.controls.addEventListener('click', showDrawer);
     ui.dismissBtn.addEventListener('click', hideDrawer)
 }
 
 // event handlers
 
-const showDrawer = (e) => {
-    ui.drawer.classList.add('show')
+const initApp = () => {
+    fetchCurrencies()
 }
 
-const hideDrawer = (e) => {
+const showDrawer = (e) => {
+    if (e.target.hasAttribute('data-drawer')) {
+        state.openDrawer = e.target.id;
+        ui.drawer.classList.add('show')
+    }
+}
+
+const hideDrawer = () => {
+    state.openDrawer = null;
     ui.drawer.classList.remove('show')
 }
 
