42303be82a02ba3b6a3989f5f6f2810cc917f972
Nasir6276 <92574698+Nasir6276@users.noreply.github.com>
2024-11-24 22:43:31 +0100

    script

---
+++ src/script.js
@@ -0,0 +1,33 @@
+// selectors
+const ui = {
+    controls: document.getElementById('controls'),
+    drawer: document.getElementById('drawer'),
+    dismissBtn: document.getElementById('dismiss-btn'),
+}
+
+// event listeners
+
+const setUpEventListeners = () => {
+    ui.controls.addEventListener('click', showDrawer);
+    ui.dismissBtn.addEventListener('click', hideDrawer)
+}
+
+// event handlers
+
+const showDrawer = (e) => {
+    ui.drawer.classList.add('show')
+}
+
+const hideDrawer = (e) => {
+    ui.drawer.classList.remove('show')
+}
+
+// render functions
+
+// helper functions
+
+// api functions
+
+// initialization
+
+setUpEventListeners()
